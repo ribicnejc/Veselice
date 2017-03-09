ab7623642cc501e90e3991a5f71b16df986b7a2a
Nejc Ribic <nejc.ribic95@gmail.com>
2017-03-09 21:46:47 +0100

    API corrections

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 include('simple_html_dom.php');
 $html = file_get_html('http://www.veselica.info/page/veselice-2017');
 $CONST = array("Petek", "Sobota", "Nedelja", "Ponedeljek", "Torek", "Sreda", "Četrtek");
-$tmp = "";
 $ar = array();
 foreach ($html->find('td#content') as $e) {
 
@@ -14,27 +13,33 @@ foreach ($html->find('td#content') as $e) {
     }
 }
 $arr = array();
-foreach ($ar as $elt){
-    if($elt == "") continue;
+foreach ($ar as $elt) {
+    if ($elt == "") continue;
     array_push($arr, $elt);
 }
 
 $perday = array();
 $main = array();
+$byDay = array();
 //print_r($arr);
-foreach ($arr as $elt){
+foreach ($arr as $elt) {
     $words = explode(", ", $elt);
-    if(in_array($words[0], $CONST)){
-        if(sizeof($perday) > 0) array_push($main, $perday);
-        array_push($main, $elt);
+
+    if (in_array($words[0], $CONST)) {
+        if (sizeof($perday) > 0) {
+            $byDay["places"] = $perday;
+            array_push($main, $byDay);
+        }
+        $byDay = array();
         $perday = array();
-    }else{
+        $byDay["date"] = $elt;
+    } else {
         array_push($perday, $elt);
     }
 }
-//print_r($main);
 
-$json = json_encode($main,JSON_UNESCAPED_UNICODE);
+
+$json = json_encode($main, JSON_UNESCAPED_UNICODE);
 echo $json;
 
 
