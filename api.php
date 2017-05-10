84ca6e3928146d97556e1c11cb59b7141f0a7d7e
Nejc Ribic <nejc.ribic95@gmail.com>
2017-05-10 21:41:35 +0200

    api fixing

--- api.php
+++ api.php
@@ -2,45 +2,50 @@
 
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$request = explode('/', trim($_SERVER['REQUEST_URI'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
-
+print_r($request);
 // connect to the mysql database
+/*
 $link = mysqli_connect('localhost', 'root', '', 'nejcribic');
-mysqli_set_charset($link,'utf8');
+mysqli_set_charset($link, 'utf8');
 
 // retrieve the table and key from the path
-$table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
-$key = array_shift($request)+0;
+$table = preg_replace('/[^a-z0-9_]+/i', '', array_shift($request));
+$key = array_shift($request) + 0;
 
 // escape the columns and values from the input object
-$columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
+$columns = preg_replace('/[^a-z0-9_]+/i', '', array_keys($input));
 $values = array_map(function ($value) use ($link) {
-    if ($value===null) return null;
-    return mysqli_real_escape_string($link,(string)$value);
-},array_values($input));
+    if ($value === null) return null;
+    return mysqli_real_escape_string($link, (string)$value);
+}, array_values($input));
 
 // build the SET part of the SQL command
 $set = '';
-for ($i=0;$i<count($columns);$i++) {
-    $set.=($i>0?',':'').'`'.$columns[$i].'`=';
-    $set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
+for ($i = 0; $i < count($columns); $i++) {
+    $set .= ($i > 0 ? ',' : '') . '`' . $columns[$i] . '`=';
+    $set .= ($values[$i] === null ? 'NULL' : '"' . $values[$i] . '"');
 }
 
 // create SQL based on HTTP method
 switch ($method) {
     case 'GET':
-        $sql = "select * from `$table`".($key?" WHERE id=$key":''); break;
+        $sql = "select * from `$table`" . ($key ? " WHERE id=$key" : '');
+        break;
     case 'PUT':
-        $sql = "update `$table` set $set where id=$key"; break;
+        $sql = "update `$table` set $set where id=$key";
+        break;
     case 'POST':
-        $sql = "insert into `$table` set $set"; break;
+        $sql = "insert into `$table` set $set";
+        break;
     case 'DELETE':
-        $sql = "delete `$table` where id=$key"; break;
+        $sql = "delete `$table` where id=$key";
+        break;
 }
 
 // excecute SQL statement
-$result = mysqli_query($link,$sql);
+$result = mysqli_query($link, $sql);
 
 // die if SQL statement failed
 if (!$result) {
@@ -51,8 +56,8 @@ if (!$result) {
 // print results, insert id or affected row count
 if ($method == 'GET') {
     if (!$key) echo '[';
-    for ($i=0;$i<mysqli_num_rows($result);$i++) {
-        echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+    for ($i = 0; $i < mysqli_num_rows($result); $i++) {
+        echo ($i > 0 ? ',' : '') . json_encode(mysqli_fetch_object($result));
     }
     if (!$key) echo ']';
 } elseif ($method == 'POST') {
@@ -62,4 +67,4 @@ if ($method == 'GET') {
 }
 
 // close mysql connection
-mysqli_close($link);
+mysqli_close($link);*/
