2c22cc8e9633d4584bf70d0f7b22ba116ebd4c6c
Nejc Ribic <nejc.ribic95@gmail.com>
2017-05-11 10:52:13 +0200

    fixed with curl, file_get_content()

--- api/myApi.php
+++ api/myApi.php
@@ -5,14 +5,31 @@
  * Date: 11. 05. 2017
  * Time: 08:36
  */
-
+include ('main.php');
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['QUERY_STRING'],'/'));
-$input = json_decode(file_get_contents('php://input'),true);
-print_r($request);
+//$input = json_decode(file_get_contents('php://input'),true);
+//print_r($request);
 
 $all = array_shift($request);
+$specific = array_shift($request);
+$key = array_shift($request);
 
-print_r(array_shift($request));
-print_r(array_shift($request));
+//echo "first: ".$all."\n";
+//echo "second: ".$specific."\n";
+//echo "third: ".$key."\n";
+
+if($all == "getAll"){
+    switch ($specific){
+        case "place":
+            break;
+        case "day":
+            break;
+        case "date":
+            break;
+        default:
+            getAll();
+            break;
+    }
+}
